a37dc4ab1ad18bd961d0563a7d186a7d3d7ca2e8
Bibek Basyal <bibekbasyal22@gmail.com>
2020-11-22 15:16:58 +0545

    url change

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@ const axios = require('axios').default;
 export const shorten = async (longUrl) => {
     let data = await axios({
         method: 'post',
-        url: '/api/url/shorten',
+        url: 'https://url-b.herokuapp.com/api/url/shorten',
         data: JSON.stringify(longUrl),
         headers: {
             'Content-Type': 'application/json',
